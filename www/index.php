12ed9937336d2d5613aa20843ff5c155b215c1bf
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2010-07-09 09:23:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/actogram@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,6 +43,24 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
+  <pre>
+require(actogram)
+data(sesa)
+# a simple actogram without left strips
+actogram(activity ~ datetime_,  dat = sesa, subset = ID == 2, strip.left = FALSE, main = "actogram")
+
+# left strips indicating the date
+actogram(activity ~ datetime_,  dat = sesa, subset = ID == 2, main = "actogram")
+
+# breeding stage information marked by the strip color
+actogram(activity ~ datetime_,  strip.left.classes = "breeding_stage", dat = sesa, subset = ID == 2, main = "actogram")
+
+# a conditional actogram using all decorations
+actogram(activity ~ datetime_,  strip.left.classes = "breeding_stage", groups = "ID" , dat = sesa, main = "conditional actogram")
+  </pre>
+
+
+
 <p> <img src="conditionalActogram.png" alt="conditional actogram" /> </p>
 <p> <img src="simpleActogram.png" alt="simple actogram" /> </p>
 
