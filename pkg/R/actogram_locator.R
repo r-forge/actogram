60f5405bfc4ecfc1eb34b16092696080bf6a79fd
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2010-11-19 20:36:23 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/actogram@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/actogram_locator.R
@@ -0,0 +1,33 @@
+
+
+
+actlocator <- function(act) {
+	
+	dev.off()
+	print(act)
+
+	day = trellis.focus()$row
+
+	id = panel.identify( n= 1, threshold = 18*2)
+
+	loc = act$panel.args[[day]]
+	loc = c(loc$x[id] , loc$y[id] )
+
+	date_ = names(act$packet.sizes[day])
+
+	datetime_ = as.POSIXct(as.POSIXlt(as.Date(date_))) + loc[1]*3600
+	
+	data.frame(act = loc[2], datetime_ = datetime_)
+	
+
+	
+}
+
+
+# data(sesa)
+# a = actogram(activity ~ datetime_,  dat = sesa, subset = ID == 2, strip.left = TRUE, main = "actogram")
+# actlocator(a)
+
+
+
+
